cda25406f0b798d0850e912c59ef47eef6a29bce
clara-pinto <clara.pinto@edu.esiee.fr>
2025-09-19 12:16:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,46 @@
 from math import sqrt
 
+
 #### Fonction secondaire
 
 
 def isprime(p):
+    '''
+    Verifie si un entier est un nombre premier ou pas
 
-    # votre code ici
+    Arg :
+       p : nombre entier
 
-    pass
+    Return :
+       retourne un booléen exprimant la vérité de « p est un nombre premier »
+
+    >>> isprime(9)
+    False
+    >>> isprime(11)
+    True
+    '''
+    premier = True
+    for i in range(2,int(sqrt(p))+1):
+        if p%i == 0:
+            premier = False
+    return premier
+   
 
 #### Fonction principale
 
 
 def main():
+    '''
+    Permet d'afficher tout les nombres premiers dans l'intervalle 0,N
+    
+    Arg : 
+       N : int
+       
+    Return : None
+    
+    >>>main(4)
+    '1,2,3'
+    '''
 
     # vos appels à la fonction secondaire ici
 
